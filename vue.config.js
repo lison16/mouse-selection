9b994d36694deda9fa0fb3ded2df54cb5c53eb4a
lizhigang.lison <lizhigang.lison@bytedance.com>
2020-03-29 19:51:29 +0800

    增加配置

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+const resolve = dir => {
+    return path.join(__dirname, dir)
+}
+module.exports = {
+    publicPath: 'https://lison16.github.io/mouse-selection/',
+    lintOnSave: true,
+    chainWebpack: config => {
+        config.resolve.alias
+            .set('@', resolve('src'))
+    },
+    // 设为false打包时不生成.map文件
+    productionSourceMap: false
+}
